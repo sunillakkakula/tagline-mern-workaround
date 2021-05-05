60b5bd4c724bfaca3db3dcc291702287668f4b94
Taglinetrader <contact_ho@taglinetraders.com>
2021-05-05 17:24:55 +0530

    Bulk, Domestic Actions..

---
+++ frontend/src/actions/bulkAction.js
@@ -0,0 +1,110 @@
+import axios from "axios";
+import {
+  BULK_LIST_BY_PRODUCT_ID_REQUEST,
+  BULK_LIST_BY_PRODUCT_ID_SUCCESS,
+  BULK_LIST_BY_PRODUCT_ID_FAIL,
+  BULK_CREATE_BY_PRODUCT_ID_REQUEST,
+  BULK_CREATE_BY_PRODUCT_ID_SUCCESS,
+  BULK_CREATE_BY_PRODUCT_ID_FAIL,
+  BULK_UPDATE_BY_PRODUCT_ID_REQUEST,
+  BULK_UPDATE_BY_PRODUCT_ID_SUCCESS,
+  BULK_UPDATE_BY_PRODUCT_ID_FAIL,
+  BULK_DELETE_BY_PRODUCT_ID_REQUEST,
+  BULK_DELETE_BY_PRODUCT_ID_SUCCESS,
+  BULK_DELETE_BY_PRODUCT_ID_FAIL,
+} from "../constants/bulkConstants";
+
+export const listBulkByProductId = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: BULK_LIST_BY_PRODUCT_ID_REQUEST });
+
+    const { data } = await axios.get(`/api/bulk/product/${id}`);
+
+    dispatch({
+      type: BULK_LIST_BY_PRODUCT_ID_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: BULK_LIST_BY_PRODUCT_ID_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const deleteBulkByProductId = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: BULK_DELETE_BY_PRODUCT_ID_REQUEST,
+    });
+
+    await axios.delete(`/api/bulk/product/${id}`);
+
+    dispatch({
+      type: BULK_DELETE_BY_PRODUCT_ID_SUCCESS,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    dispatch({
+      type: BULK_DELETE_BY_PRODUCT_ID_FAIL,
+      payload: message,
+    });
+  }
+};
+
+export const createBulkByProductId = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: BULK_CREATE_BY_PRODUCT_ID_REQUEST,
+    });
+
+    const { data } = await axios.post(`/api/bulk/product/${id}`, {});
+
+    dispatch({
+      type: BULK_CREATE_BY_PRODUCT_ID_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+
+    dispatch({
+      type: BULK_CREATE_BY_PRODUCT_ID_FAIL,
+      payload: message,
+    });
+  }
+};
+
+export const updateBulkByProductId = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: BULK_UPDATE_BY_PRODUCT_ID_REQUEST,
+    });
+
+    const { data } = await axios.put(`/api/bulk/product/${id}`);
+
+    dispatch({
+      type: BULK_UPDATE_BY_PRODUCT_ID_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    if (message === "Not authorized, token failed") {
+    }
+    dispatch({
+      type: BULK_UPDATE_BY_PRODUCT_ID_FAIL,
+      payload: message,
+    });
+  }
+};
---
+++ frontend/src/actions/domesticAction.js
@@ -0,0 +1,110 @@
+import axios from "axios";
+import {
+  DOMESTIC_LIST_BY_PRODUCT_ID_REQUEST,
+  DOMESTIC_LIST_BY_PRODUCT_ID_SUCCESS,
+  DOMESTIC_LIST_BY_PRODUCT_ID_FAIL,
+  DOMESTIC_CREATE_BY_PRODUCT_ID_REQUEST,
+  DOMESTIC_CREATE_BY_PRODUCT_ID_SUCCESS,
+  DOMESTIC_CREATE_BY_PRODUCT_ID_FAIL,
+  DOMESTIC_UPDATE_BY_PRODUCT_ID_REQUEST,
+  DOMESTIC_UPDATE_BY_PRODUCT_ID_SUCCESS,
+  DOMESTIC_UPDATE_BY_PRODUCT_ID_FAIL,
+  DOMESTIC_DELETE_BY_PRODUCT_ID_REQUEST,
+  DOMESTIC_DELETE_BY_PRODUCT_ID_SUCCESS,
+  DOMESTIC_DELETE_BY_PRODUCT_ID_FAIL,
+} from "../constants/domesticConstants";
+
+export const listDomesticByProductId = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: DOMESTIC_LIST_BY_PRODUCT_ID_REQUEST });
+
+    const { data } = await axios.get(`/api/domestic/product/${id}`);
+
+    dispatch({
+      type: DOMESTIC_LIST_BY_PRODUCT_ID_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: DOMESTIC_LIST_BY_PRODUCT_ID_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const deleteDomesticByProductId = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: DOMESTIC_DELETE_BY_PRODUCT_ID_REQUEST,
+    });
+
+    await axios.delete(`/api/domestic/product/${id}`);
+
+    dispatch({
+      type: DOMESTIC_DELETE_BY_PRODUCT_ID_SUCCESS,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    dispatch({
+      type: DOMESTIC_DELETE_BY_PRODUCT_ID_FAIL,
+      payload: message,
+    });
+  }
+};
+
+export const createDomesticByProductId = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: DOMESTIC_CREATE_BY_PRODUCT_ID_REQUEST,
+    });
+
+    const { data } = await axios.post(`/api/domestic/product/${id}`, {});
+
+    dispatch({
+      type: DOMESTIC_CREATE_BY_PRODUCT_ID_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+
+    dispatch({
+      type: DOMESTIC_CREATE_BY_PRODUCT_ID_FAIL,
+      payload: message,
+    });
+  }
+};
+
+export const updateDomesticByProductId = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: DOMESTIC_UPDATE_BY_PRODUCT_ID_REQUEST,
+    });
+
+    const { data } = await axios.put(`/api/domestic/product/${id}`);
+
+    dispatch({
+      type: DOMESTIC_UPDATE_BY_PRODUCT_ID_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    if (message === "Not authorized, token failed") {
+    }
+    dispatch({
+      type: DOMESTIC_UPDATE_BY_PRODUCT_ID_FAIL,
+      payload: message,
+    });
+  }
+};
